cf71d4bc114640527fab8a5e265e8f3408d837cb
rebelsky <rebelsky@grinnell.edu>
2013-07-17 22:46:06 -0500

    tile-core.c now has mostly working functions that fill and recompute a given drawable.  (More testing is probably needed, but they look pretty good right now.)

--- tile-core.c
+++ tile-core.c
@@ -41,20 +41,37 @@
 #include <scheme.h>
 
 
+// +---------+---------------------------------------------------------
+// | Globals |
+// +---------+
+
+/**
+ * The loudbus_call function.  
+ */
+static Scheme_Object *loudbus_call = NULL;
+
+/**
+ * The gimpplus object.
+ */
+static Scheme_Object *gimpplus = NULL;
+
+
 // +-----------------+-------------------------------------------------
 // | Local Utilities |
 // +-----------------+
 
 /**
- * Shorten an integer to one byte.
+ * Add a procedure
  */
-static int
-byte (int x)
+static void
+register_scheme_function (Scheme_Prim *prim, char *name,
+                          int minarity, int maxarity,
+                          Scheme_Env *menv)
 {
-  if (x < 0) return 0;
-  if (x > 255) return 255;
-  return x;
-} // byte
+  Scheme_Object *proc = 
+    scheme_make_prim_w_arity (prim, name, minarity, maxarity);
+  scheme_add_global (name, proc, menv);
+} // register_scheme_function
 
 /**
  * Extract information from a tile.
@@ -87,29 +104,110 @@ unpack_tile (Scheme_Object *tile,
 } // unpack_tile
 
 
+// +-----------------------+-------------------------------------------
+// | Integer RGB Functions |
+// +-----------------------+
+
+#include "irgb-funs.c"
+
+
+// +----------------+--------------------------------------------------
+// | Tile Functions |
+// +----------------+
+
+int
+tile_stream_advance (Scheme_Object *stream)
+{
+  static Scheme_Object *params[4];
+  params[0] = gimpplus;
+  params[1] = scheme_make_symbol ("tile_stream_advance");
+  params[2] = stream;
+  params[3] = NULL;
+  Scheme_Object *result = scheme_apply (loudbus_call, 3, params);
+  return SCHEME_INT_VAL (SCHEME_CAR (result));
+} // tile_stream_advance
+
+int
+tile_stream_close (Scheme_Object *stream)
+{
+  static Scheme_Object *params[4];
+  params[0] = gimpplus;
+  params[1] = scheme_make_symbol ("tile_stream_close");
+  params[2] = stream;
+  params[3] = NULL;
+  return SCHEME_INT_VAL (SCHEME_CAR (scheme_apply (loudbus_call, 3, params)));
+} // tile_stream_close
+
+Scheme_Object *
+tile_stream_get (Scheme_Object *stream)
+{
+  static Scheme_Object *params[4];
+  params[0] = gimpplus;
+  params[1] = scheme_make_symbol ("tile_stream_get");
+  params[2] = stream;
+  params[3] = NULL;
+  return scheme_apply (loudbus_call, 3, params);
+} // tile_stream_get
+
+Scheme_Object *
+tile_stream_new (Scheme_Object *image, Scheme_Object *drawable)
+{
+  static Scheme_Object *params[5];
+  params[0] = gimpplus;
+  params[1] = scheme_make_symbol ("tile_stream_new");
+  params[2] = image;
+  params[3] = drawable;
+  params[4] = NULL;	// May not be necessary
+  return SCHEME_CAR (scheme_apply (loudbus_call, 4, params));
+} // tile_stream_new
+
+int
+tile_update (Scheme_Object *stream, int size, unsigned char *data)
+{
+  static Scheme_Object *params[6];
+  params[0] = gimpplus;
+  params[1] = scheme_make_symbol ("tile_update");
+  params[2] = stream;
+  params[3] = scheme_make_integer (size);
+  params[4] = scheme_make_sized_byte_string (data, size, 1);
+  params[5] = NULL;
+  return SCHEME_INT_VAL (SCHEME_CAR (scheme_apply (loudbus_call, 5, params)));
+} // tile_update
+
+
 // +------------+------------------------------------------------------
 // | Extensions |
 // +------------+
 
 /**
- * Fill an image with a single color.  Intended primarily as a test
+ * Initialize.
+ */
+Scheme_Object *
+tile_core_init (int argc, Scheme_Object **argv)
+{
+  loudbus_call = argv[0];
+  gimpplus = argv[1];
+  scheme_register_static (&loudbus_call, sizeof (Scheme_Object *));
+  scheme_register_static (&gimpplus, sizeof (Scheme_Object *));
+  return scheme_void;
+} // tile_core_init
+
+/**
+ * Fill a drawable with a single color.  Intended primarily as a test
  * of tiles.
  * 
  * Parameters:
- *   1: loudbus-call
- *   2: gimp
- *   3: image-id
- *   4: drawable-id
- *   5: color
+ *   0: image-id
+ *   1: drawable-id
+ *   2: color
  */
 Scheme_Object *
-image_fill_core (int argc, Scheme_Object **argv)
+drawable_fill_core (int argc, Scheme_Object **argv)
 {
-  Scheme_Object *params[16];	// Handy array of parameters
-  Scheme_Object *loudbus_call;	// The call function
   Scheme_Object *stream = NULL;	// The tile stream
   Scheme_Object *tile = NULL;	// Info on the current tile
-  int more_tiles = 1;		// Are there more tiles?
+  int color;                    // The color as an iRGB
+  unsigned char r, g, b;                  // The color components
 
   int tsize;
   unsigned char *tdata;
@@ -120,52 +218,230 @@ image_fill_core (int argc, Scheme_Object **argv)
   int tw;
   int th;
 
-  // Check parameters
-  // STUB
+  // Sanity check
+  if (! SCHEME_INTP (argv[0]))
+    {
+      scheme_wrong_type ("drawable-fill!", "image", 0, argc, argv);
+      return scheme_void;
+    }
+  if (! SCHEME_INTP (argv[1]))
+    {
+      scheme_wrong_type ("drawable-fill!", "drawable", 0, argc, argv);
+      return scheme_void;
+    }
+  if (! SCHEME_INTP (argv[0]))
+    {
+      scheme_wrong_type ("drawable-fill!", "iRGB", 0, argc, argv);
+      return scheme_void;
+    }
 
-  // Grab the call function
-  loudbus_call = argv[0];
+  // Set up for garbage collection
+  MZ_GC_DECL_REG (1);
+  MZ_GC_VAR_IN_REG (0, stream);
+  MZ_GC_REG ();
 
-  // Set up the default parameters
-  params[0] = argv[1];	// gimp
+  // Get the color
+  color  = SCHEME_INT_VAL (argv[2]);
+  r = (unsigned char) irgb_red (color);
+  g = (unsigned char) irgb_green (color);
+  b = (unsigned char) irgb_blue (color);
 
   // Get a stream
-  params[1] = scheme_make_symbol ("tile_stream_new");
-  params[2] = argv[2];	// image
-  params[3] = argv[3];	// drawable
-  params[4] = NULL;	// May not be necessary
-  stream = SCHEME_CAR (scheme_apply (loudbus_call, 4, params));
-  
+  stream = tile_stream_new (argv[0], argv[1]);
+
   // Check the validity of the stream
   // STUB
 
   // Get all the tiles 
   do {
-    // Get a tile
-    params[1] = scheme_make_symbol ("tile_stream_get");
-    params[2] = stream;
-    params[3] = NULL;
-    tile = scheme_apply (loudbus_call, 3, params);
+    // Get the data from the current tile 
+    tile = tile_stream_get (stream);
     unpack_tile (tile, &tsize, &tdata, &tbpp, &trowstride, &tx, &ty, &tw, &th);
+
     // Process the tile
-    fprintf (stderr, "size:%d, bpp:%d, rowstride:%d, x:%d, y:%d, w:%d, h:%d\n",
-             tsize, tbpp, trowstride, tx, ty, tw, th);
-    // Advance to the next tile
-    params[1] = scheme_make_symbol ("tile_stream_advance");
-    params[2] = stream;
-    params[3] = NULL;
-    more_tiles = SCHEME_INT_VAL (SCHEME_CAR (scheme_apply (loudbus_call, 3, params)));
-  } while (more_tiles);
+    unsigned char *tmp = tdata;
+    // Do each row
+    int row;
+    for (row = 0; row < th; row++)
+      {
+        // Grab a pointers to the start of the data
+        unsigned char *t = tmp;
+
+        // Do each column in the row
+        int col;
+        for (col = 0; col < tw; col++)
+          {
+            t[0] = r;
+            t[1] = g;
+            t[2] = b;
+
+            // Advance to the next pixel
+            t += tbpp;
+          } // for each column
+
+        // Advance to the next row
+        tmp += trowstride;
+      } // for each row
+
+    // Push the modified tile back to the server
+    tile_update (stream, tsize, tdata);
+  } while (tile_stream_advance (stream));
 
   // Close the stream
-  params[1] = scheme_make_symbol ("tile_stream_close");
-  params[2] = stream;
-  params[3] = NULL;
-  tile = scheme_apply (loudbus_call, 3, params);
+  tile_stream_close (stream);
+
+  // Clean up
+  MZ_GC_UNREG ();
 
   // And we're done
-  return argv[2];
-} // image_fill_core
+  return argv[0];
+} // drawable_fill_core
+
+/**
+ * Recompute the pixels in a drawable.  (Inner code for image-compute! and
+ * other functions.)
+ * 
+ * Parameters:
+ *   0: image-id
+ *   1: drawable-id
+ *   2: lambda (x,y) -> color
+ */
+Scheme_Object *
+drawable_recompute_core (int argc, Scheme_Object **argv)
+{
+  Scheme_Object *stream = NULL;	// The tile stream
+  Scheme_Object *tile = NULL;	// Info on the current tile
+  Scheme_Object *fun  = NULL;   // The color function
+  Scheme_Object *pos[2] = { NULL, NULL };
+                                // The current position
+  int color;                    // The color as an iRGB
+  unsigned char r, g, b;        // The color components
+
+  int tsize;
+  unsigned char *tdata;
+  int tbpp;
+  int trowstride;
+  int tx;
+  int ty;
+  int tw;
+  int th;
+
+  // Set up for garbage collection
+  MZ_GC_DECL_REG (4);
+  MZ_GC_VAR_IN_REG (0, stream);
+  MZ_GC_VAR_IN_REG (1, fun);
+  MZ_GC_VAR_IN_REG (2, pos[0]);
+  MZ_GC_VAR_IN_REG (3, pos[1]);
+  MZ_GC_REG ();
+
+  // Get the function
+  fun = argv[2];
+
+  // Get a stream
+  stream = tile_stream_new (argv[0], argv[1]);
+
+  // Check the validity of the stream
+  // STUB
+
+  // Get all the tiles 
+  do {
+    // Get the data from the current tile 
+    tile = tile_stream_get (stream);
+    unpack_tile (tile, &tsize, &tdata, &tbpp, &trowstride, &tx, &ty, &tw, &th);
+
+    // Process the tile
+    unsigned char *tmp = tdata;
+    // Do each row
+    int row;
+    for (row = 0; row < th; row++)
+      {
+        // Grab a pointer to the start of the data
+        unsigned char *t = tmp;
+        // Set up half of the position
+        pos[1] = scheme_make_integer (row + ty);
+
+        // Do each column in the row
+        int col;
+        for (col = 0; col < tw; col++)
+          {
+            pos[0] = scheme_make_integer (col + tx);
+            color = SCHEME_INT_VAL (scheme_apply (fun, 2, pos));
+/*
+            t[0] = (unsigned char) irgb_red (color);
+            t[1] = (unsigned char) irgb_green (color);
+            t[2] = (unsigned char) irgb_blue (color);
+ */
+            t[0] = IRGB_RED (color);
+            t[1] = IRGB_GREEN (color);
+            t[2] = IRGB_BLUE (color);
+
+            // Advance to the next pixel
+            t += tbpp;
+          } // for each column
+
+        // Advance to the next row
+        tmp += trowstride;
+      } // for each row
+
+    // Push the modified tile back to the server
+    tile_update (stream, tsize, tdata);
+  } while (tile_stream_advance (stream));
+
+  // Close the stream
+  tile_stream_close (stream);
+
+  // Clean up
+  MZ_GC_UNREG ();
+
+  // And we're done
+  return argv[0];
+} // drawable_recompute_core
+
+
+// +-------------+-----------------------------------------------------
+// | Experiments |
+// +-------------+
+
+/**
+ * Fill a drawable with a single color.  Intended primarily as a test
+ * of tiles.  (This extended version takes loudbus_call and gimp as
+ * parameters so that I can check where errors might occur.
+
+ * 
+ * Parameters:
+ *   0: loudbus-call
+ *   1: gimp
+ *   2: image-id
+ *   3: drawable-id
+ *   4: color
+ */
+Scheme_Object *
+drawable_fill_extended (int argc, Scheme_Object **argv)
+{
+  Scheme_Object *result = scheme_void;
+
+  // Check parameters
+  // STUB
+
+  // Grab the call function
+  loudbus_call = argv[0];
+  gimpplus = argv[1];
+ 
+  // Set up for garbage collection
+  MZ_GC_DECL_REG (2);
+  MZ_GC_VAR_IN_REG (0, loudbus_call);
+  MZ_GC_VAR_IN_REG (1, gimpplus);
+  MZ_GC_REG ();
+
+  // Do the real computation
+  result = drawable_fill_core (argc-2, argv+2);
+
+  // Clean up
+  MZ_GC_UNREG ();
+
+  // And we're done
+  return result;
+} // drawable_fill_extended
 
 /**
  * Build a new image using a function from positions to colors.
@@ -230,8 +506,10 @@ scheme_reload (Scheme_Env *env)
   menv = scheme_primitive_module (scheme_intern_symbol ("tile-core"), env);
 
   // Add the procedures
-  proc = scheme_make_prim_w_arity (image_fill_core, "image-fill-core", 5, 5);
-  scheme_add_global ("image-fill-core", proc, menv);
+  register_scheme_function (drawable_fill_extended, "drawable-fill-extended", 5, 5, menv);
+  register_scheme_function (drawable_fill_core, "_drawable-fill!", 3, 3, menv);
+  register_scheme_function (drawable_recompute_core, "_drawable-recompute!", 3, 3, menv);
+  register_scheme_function (tile_core_init, "tile-core-init", 2, 2, menv);
 
   // Clean up
   scheme_finish_primitive_module (menv);
