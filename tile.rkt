aebaafd2b660bf4520163609a02da82918199c00
rebelsky <rebelsky@grinnell.edu>
2013-07-17 22:47:23 -0500

    tile.rkt updated to correspond to the changes in tile-core.rkt.

--- tile.rkt
+++ tile.rkt
@@ -3,15 +3,62 @@
 ;;; tile.rkt
 ;;;   A wrapper for the tile functions
 
-(require "tile-core")
-(require louDBus/unsafe)
-(provide (all-defined-out))
+(require "tile-core")		; Functions we're wrapping
 
+(require louDBus/unsafe 	; For loudbus_call
+         LoudGimp/gimp-dbus 	; Get access to the PDB
+         LoudGimp/guard 	; For guarding stuff
+         LoudGimp/higher 	; For constant
+         LoudGimp/image 	; For image?
+         LoudGimp/drawable 	; For drawable?
+         LoudGimp/irgb 		; For irgb-new
+ )
+
+(provide (all-defined-out)
+         (all-from-out "tile-core"))
+
+; Set up the dbus object to call.
 (define gimpplus (loudbus-proxy "edu.grinnell.cs.glimmer.GimpDBus"
                                 "/edu/grinnell/cs/glimmer/gimp"
                                 "edu.grinnell.cs.glimmer.gimpplus"))
 
-(define image-fill!
-  (lambda (image drawable color)
-    (image-fill-core loudbus-call gimpplus image drawable color)))
+; +----------------+--------------------------------------------------
+; | Initialization |
+; +----------------+
 
+(tile-core-init loudbus-call gimpplus)
+
+; +----------+--------------------------------------------------------
+; | Wrappers |
+; +----------+
+
+;(define drawable-fill!
+;  (lambda (image drawable color)
+;    (display "Filling drawable.\n")
+;    (drawable-fill-core loudbus-call gimpplus image drawable color)))
+
+(define drawable-fill!
+  (guard-proc 'drawable-fill!
+              _drawable-fill!
+              (list 'image 'drawable 'irgb-color)
+              (list image-id? drawable? integer?)))
+
+(define drawable-recompute!
+  (guard-proc 'drawable-recompute!
+              _drawable-recompute!
+	      (list 'image 'drawable 'pos2color)
+	      (list image-id? drawable? (constant #t))))
+
+(define _image-compute!
+  (lambda (pos2color width height)
+    (let* ((image (image-new width height))
+           (drawable (image-get-layer image)))
+      (_drawable-recompute! image drawable pos2color))))
+      
+(define image-compute!
+  (guard-proc 'image-compute!
+              _image-compute!
+	      (list 'pos2color 'positive-integer 'positive-integer )
+	      (list (constant #t)
+	            (and integer? exact? positive?)
+		    (and integer? exact? positive?))))
