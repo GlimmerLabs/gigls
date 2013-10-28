2585600999bbf2fc768b6cb8d25a04b919e20d38
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-10-27 20:22:51 -0500

    Add fun strings procedures to gigls.

--- strings.rkt
+++ strings.rkt
@@ -22,6 +22,7 @@
 ;   License along with gigls.  If not, see <http://www.gnu.org/licenses/>.
 
 (provide (all-defined-out))
+(require gigls/guard)
 
 ;;; Procedure:
 ;;;   string-ends-with?
@@ -32,12 +33,79 @@
 ;;;   Determine if string ends with suffix
 ;;; Produces:
 ;;;   is-suffix?, a Boolean
-(define string-ends-with?
+(define _string-ends-with?
   (lambda (string suffix)
     (let ([string-len (string-length string)]
           [suffix-len (string-length suffix)])
       (and (>= string-len suffix-len)
            (string=? (substring string (- string-len suffix-len)) suffix)))))
-           
 
+(define string-ends-with?
+  (guard-proc 'string-ends-with?
+              _string-ends-with?
+              (list 'string 'string)
+              (list string? string?)))
+
+;;; Procedure:
+;;;   string-remove-leading-chars
+;;; Parameters:
+;;;   str, a string
+;;;   char-pred?, a character predicate
+;;; Purpose:
+;;;   Remove all of the leading characters from str for which pred holds
+;;; Produces:
+;;;   newstr, a string
+;;; Postconditions:
+;;;   Exists i s.t. newstr is (substring str i).
+;;;   For all j, 0 <= j < i, (char-pred? (string-ref str j)).
+;;;   (not (char-pred? (string-ref str i))).
+(define _string-remove-leading-chars
+  (lambda (str char-pred?)
+    (let kernel ([i 0]
+                 [len (string-length str)])
+      (cond
+        [(>= i len)
+         ""]
+        [(char-pred? (string-ref str i))
+         (kernel (+ i 1) len)]
+        [else
+         (substring str i)]))))
+
+(define string-remove-leading-chars
+  (guard-proc 'string-remove-leading-chars
+              _string-remove-leading-chars
+              (list 'string 'character-predicate)
+              (list string? procedure?)))
+
+;;; Procedure:
+;;;   string-remove-trailing-chars
+;;; Parameters:
+;;;   str, a string
+;;;   char-pred?, a character predicate
+;;; Purpose:
+;;;   Remove all of the trailing characters from str for which pred holds
+;;; Produces:
+;;;   newstr, a string
+;;; Postconditions:
+;;;   Exists i s.t. newstr is (substring str 0 i)
+;;;   For all j, i <= j < (string-length str), 
+;;;     (char-pred? (string-ref str j))
+;;;   (not (char-pred? (string-ref str (- i 1))))
+(define _string-remove-trailing-chars
+  (lambda (str char-pred?)
+    (let ([len (string-length str)])
+      (let kernel ([i (- len 1)])
+        (cond
+          [(< i 0)
+           ""]
+          [(char-pred? (string-ref str i))
+           (kernel (- i 1))]
+          [else
+           (substring str 0 (+ i 1))])))))
+
+(define string-remove-trailing-chars
+  (guard-proc 'string-remove-trailing-chars
+              _string-remove-trailing-chars
+              (list 'string 'character-predicate)
+              (list string? procedure?)))
 
